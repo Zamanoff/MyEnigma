caf2a8c3daa3a00b77c3cca590861286416305f8
Zamanoff <egoyuncu@gmail.com>
2019-03-01 10:49:40 +0400

    MyEnigma

--- MyEnigma/Form1.Designer.cs
+++ MyEnigma/Form1.Designer.cs
@@ -28,13 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PatchBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PassTextBox = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PatchBox
+            // 
+            this.PatchBox.Location = new System.Drawing.Point(106, 16);
+            this.PatchBox.Name = "PatchBox";
+            this.PatchBox.Size = new System.Drawing.Size(310, 20);
+            this.PatchBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Пароль";
+            // 
+            // PassTextBox
+            // 
+            this.PassTextBox.Location = new System.Drawing.Point(106, 44);
+            this.PassTextBox.Name = "PassTextBox";
+            this.PassTextBox.PasswordChar = '*';
+            this.PassTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PassTextBox.TabIndex = 3;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(223, 46);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(95, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Зашифровать";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(325, 46);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(101, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.Text = "Расшифровать";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 84);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(400, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(243, 124);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 7;
+            this.StartButton.Text = "Пуск";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // button3
+            // 
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button3.Location = new System.Drawing.Point(341, 124);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Отмена";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(435, 160);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.PassTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PatchBox);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox PatchBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PassTextBox;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- MyEnigma/Form1.cs
+++ MyEnigma/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,89 @@ namespace MyEnigma
 {
     public partial class Form1 : Form
     {
+        public string text;
+        public string patch;
         public Form1()
         {
             InitializeComponent();
+        }
+        private static string Encrypt(string message, int key, string pass)
+        {
+            string result = "";
+            for (int i = 0; i < pass.Length; i++)
+                result += (char)(pass[i] ^ key);
+
+
+            for (int i = pass.Length; i < message.Length; i++)
+                result += (char)(message[i] ^ key);
+            return result;
+        }
+
+        private static string Decrypt(string message, int key, string pass )
+        {
+            string encryptText = "";
+                for (int i = 0; i < message.Length; i++)
+                encryptText += (char)(message[i] ^ key);
+
+            if (encryptText.Substring(0, pass.Length) == pass)
+            {
+                encryptText.Remove(0, pass.Length);
+                return encryptText;
+            }
+            else return "Incorrect password";
+
+            //return encryptText;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                patch = fileDialog.FileName;
+                PatchBox.Text = patch;
+
+                text = File.ReadAllText(fileDialog.FileName, Encoding.Default);
+            }
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            PatchBox.Clear();
+            progressBar1.Value = 100;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            var task = new Task(() =>
+            {
+                if (radioButton1.Checked)
+                {
+                    using (StreamWriter sw = new StreamWriter(patch, false, Encoding.Default))
+                    {
+                        sw.WriteLine(Encrypt(text, 5, PassTextBox.Text));
+                        PassTextBox.Clear();
+
+                    }
+                }
+                else if (radioButton2.Checked && Decrypt(text, 5, PassTextBox.Text) != "Incorrect password")
+                {
+                    using (StreamWriter sw = new StreamWriter(patch, false, Encoding.Default))
+                    {
+                        sw.WriteLine(Decrypt(text, 5, PassTextBox.Text));
+                        PassTextBox.Clear();
+                    }
+                }
+                
+            }
+            );
+            task.Start();
+            task.ContinueWith((x) => {
+                progressBar1.Style = ProgressBarStyle.Blocks;
+                MessageBox.Show("Complete"); });
+           if (task.IsFaulted)
+            {
+                MessageBox.Show("Incorrect Password");
+            }
         }
     }
 }
